7b3180ffafc40b1ea4b5fdf63bd8fb8eb33eef2f
Andrea Manzini <ilmanzo@gmail.com>
2020-12-14 17:02:56 +0100

    add help and command line handling via the 'flag' package

--- go.mod
+++ go.mod
@@ -4,8 +4,7 @@ go 1.15
 
 require (
 	github.com/gdamore/tcell/v2 v2.0.1-0.20201017141208-acf90d56d591
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/rivo/tview v0.0.0-20201018122409-d551c850a743
-	golang.org/x/sys v0.0.0-20201110211018-35f3e6cf4a65 // indirect
+	github.com/rivo/tview v0.0.0-20201204190810-5406288b8e4e
+	golang.org/x/sys v0.0.0-20201214095126-aec9a390925b // indirect
 	golang.org/x/text v0.3.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,40 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 
 	"github.com/singalhimanshu/taskgo/files"
 	"github.com/singalhimanshu/taskgo/ui"
 )
 
+const defaultFileName = "taskgo.md"
+
 func main() {
-	fileName := "taskgo.md"
-	args := os.Args
-	if len(args) == 2 {
-		fileName = args[1]
-		if !strings.HasSuffix(fileName, ".md") {
-			log.Fatal("Invalid file extension (make sure it is a .md(markdown) file)")
-		}
+	fileName := flag.String("f", defaultFileName, "markdown file to use as task storage")
+	flag.Parse()
+	if !strings.HasSuffix(*fileName, ".md") {
+		log.Fatal("Invalid file extension (make sure it is a .md(markdown) file)")
 	}
-	checkFile := files.CheckFile("/" + fileName)
+	checkFile := files.CheckFile("/" + *fileName)
 	if !checkFile {
-		fmt.Printf("%q doesn't exist. Do you want to create it? (Y[es]/n[o]) ", fileName)
+		fmt.Printf("%q doesn't exist. Do you want to create it? (Y[es]/n[o]) ", *fileName)
 
 		var createFile string
 		fmt.Scanln(&createFile)
 
 		if createFile == "y" || createFile == "Y" || createFile == "Yes" {
-			files.CreateFile("/" + fileName)
-			files.WriteInitialContent("/" + fileName)
+			files.CreateFile("/" + *fileName)
+			files.WriteInitialContent("/" + *fileName)
 		} else {
 			return
 		}
 	}
 
-	err := ui.Start("/" + fileName)
+	err := ui.Start("/" + *fileName)
 
 	if err != nil {
 		panic(err)
