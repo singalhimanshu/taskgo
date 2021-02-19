511a3a13fc81cc429717aef440e8108a86e93521
Himanshu <himansingh24@gmail.com>
2021-02-19 22:46:54 +0530

    test: add test for remove task command

--- command/command_test.go
+++ command/command_test.go
@@ -18,6 +18,12 @@ func TestExecute(t *testing.T) {
 	if err := testData.ParseData(tempFileContent); err != nil {
 		t.Errorf("Unexpected Error: %v", err)
 	}
+	testData = &parser.Data{}
+	if err := testData.ParseData(tempFileContent); err != nil {
+		t.Errorf("Unexpected Error: %v", err)
+	}
+	testCommand = CreateNewCommand(testData)
+	t.Run("Remove task Command", testRemoveTaskCommand(t, testCommand, testData))
 }
 
 func testAddTaskCommand(t *testing.T, testCommand *CommandManager, testData *parser.Data) func(*testing.T) {
@@ -56,6 +62,47 @@ func testAddTaskCommand(t *testing.T, testCommand *CommandManager, testData *par
 		}
 		if err := compareTaskData(actualTaskTitle, actualTaskDesc, gotTaskData[0], gotTaskData[1]); err != nil {
 			t.Error(err)
+		}
+	}
+}
+
+func testRemoveTaskCommand(t *testing.T, testCommand *CommandManager, testData *parser.Data) func(*testing.T) {
+	return func(t *testing.T) {
+		t.Helper()
+		listIdx, taskIdx := 0, 0
+		actualTaskTitle := "test1"
+		actualTaskDesc := "test1 desc"
+		addTaskCommand := CreateAddTaskCommand(listIdx, actualTaskTitle, actualTaskDesc, taskIdx)
+		removeTaskCommand := CreateRemoveTaskCommand(listIdx, taskIdx)
+		// Test Execute
+		if err := testCommand.Execute(addTaskCommand); err != nil {
+			t.Errorf("Unexpected Error: %v", err)
+		}
+		if err := testCommand.Execute(removeTaskCommand); err != nil {
+			t.Errorf("Unexpected Error: %v", err)
+		}
+		_, err := testData.GetTask(listIdx, taskIdx)
+		if err == nil {
+			t.Error("Expected error, but didn't get one")
+		}
+		// Test Undo
+		if err := testCommand.Undo(); err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		gotTaskData, err := testData.GetTask(listIdx, taskIdx)
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		if err := compareTaskData(actualTaskTitle, actualTaskDesc, gotTaskData[0], gotTaskData[1]); err != nil {
+			t.Error(err)
+		}
+		// Test Redo
+		if err := testCommand.Redo(); err != nil {
+			t.Error(err)
+		}
+		_, err = testData.GetTask(listIdx, taskIdx)
+		if err == nil {
+			t.Error("Expected error, but didn't get one")
 		}
 	}
 }
